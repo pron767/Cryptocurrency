422bf99674e965036c8c327cf08ddf5669f31eb2
IvanxMir <miroslavskix11@gmail.com>
2021-04-08 08:03:14 +0300

    Initial commit

---
+++ app/src/main/java/com/example/nextday/ActivityFragment.kt
@@ -0,0 +1,17 @@
+package com.example.nextday
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.FragmentActivity
+
+class ActivityFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_activity, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/nextday/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.nextday
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sign_up)
+    }
+}
